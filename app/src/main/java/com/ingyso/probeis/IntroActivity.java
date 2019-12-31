cbed27152c9a8109b93a8360bc51f4dfbb33535d
Omar Navarro <omar.navarroy@yahoo.com>
2019-12-30 18:01:47 -0800

    Added Capture screen frame and clicklistener when user is captura

--- app/src/main/java/com/ingyso/probeis/IntroActivity.java
+++ app/src/main/java/com/ingyso/probeis/IntroActivity.java
@@ -13,6 +13,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
--- app/src/main/java/com/ingyso/probeis/LoginActivity.java
+++ app/src/main/java/com/ingyso/probeis/LoginActivity.java
@@ -1,15 +1,49 @@
 package com.ingyso.probeis;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ingyso.probeis.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button loginButton;
+    private TextView usernameText;
+    private TextView passwordtext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.loginButton);
+        usernameText = findViewById(R.id.username);
+        passwordtext = findViewById(R.id.password);
+
+        //---------------- OnClickListeners ---------------------
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(LoginActivity.this, "Authentication failed.",
+//                        Toast.LENGTH_SHORT).show();
+                // Si el usuario es para captura de metricas, mostramos la pantalla de captura de metricas.
+                if ((usernameText.getText() == "captura") && (passwordtext.getText() == "captura")) {
+                    //open main activity
+                    Intent captureActivity = new Intent(getApplicationContext(), CaptureActivity.class);
+                    startActivity(captureActivity);
+                    // also we need to save a boolean value to storage so next time when the user run the app
+                    // we could know that he is already checked the intro screen activity
+                    // i'm going to use shared preferences to that process
+                    //savePrefsData();
+                    finish();
+                }
+            }
+        });
     }
 }
